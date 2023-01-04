cb923b74ff81f2368d523e469408c0c2f54ef486
Eloísa dos Anjos <eldanjos5351@gmail.com>
2023-01-03 22:16:21 -0300

    VERY BAD SOLUTION

---
+++ src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entitites.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.println("Room Number: ");
+		int roomNumber = sc.nextInt();
+		System.out.println("Check in: [dd/MM/yyyy]");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.println("Check out: [dd/MM/yyyy]");
+		Date checkOut = sdf.parse(sc.next());
+
+		if (!checkOut.after(checkIn)) {
+			System.out.println("***ERROR IN RESERVATION*** \nCheck Out date must be after Check In date");
+		} else {
+			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+
+			System.out.println();
+			System.out.println("UPDATE RESERVATION: ");
+			System.out.println("Check in: [dd/MM/yyyy]");
+			checkIn = sdf.parse(sc.next());
+			System.out.println("Check out: [dd/MM/yyyy]");
+			checkOut = sdf.parse(sc.next());
+
+			Date now = new Date();
+			if (checkIn.before(now) | checkOut.before(now)) {
+				System.out.println("***ERROR IN RESERVATION*** \nReservations dates for update must be future dates");
+
+			} else if (!checkOut.after(checkIn)) {
+				System.out.println("***ERROR IN RESERVATION*** \nCheck Out date must be after Check In date");
+
+			} else {
+
+				reservation.update(checkIn, checkOut);
+				System.out.println("RESERVATION: " + reservation);
+			}
+		}
+
+	}
+
+}
